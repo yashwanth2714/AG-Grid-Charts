fb8d6ea7c2acd41959557dc7042b54d757754f4d
Guntur, Yashwanth <yashwanthkumarguntur5@gmail.com>
2021-12-12 16:17:56 +0530

    ag-grid line series

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,85 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="app">
+    <ag-charts-vue :options="options"></ag-charts-vue>
   </div>
 </template>
 
 <script>
+import { AgChartsVue } from "ag-charts-vue";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  components: {
+    AgChartsVue,
+  },
+  data: function () {
+    return {
+      options: null,
+    };
+  },
+  created() {
+    this.options = {
+      autoSize: true,
+      title: { text: "Fuel Spending (2019)" },
+      data: [
+        {
+          quarter: "Q1",
+          petrol: 200,
+          diesel: 100,
+        },
+        {
+          quarter: "Q2",
+          petrol: 300,
+          diesel: 130,
+        },
+        {
+          quarter: "Q3",
+          petrol: 350,
+          diesel: 160,
+        },
+        {
+          quarter: "Q4",
+          petrol: 400,
+          diesel: 200,
+        },
+      ],
+      series: [
+        {
+          xKey: "quarter",
+          yKey: "petrol",
+        },
+        {
+          xKey: "quarter",
+          yKey: "diesel",
+        },
+      ],
+    };
+  },
+  mounted() {},
+  methods: {},
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+html,
+body,
+#app {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  box-sizing: border-box;
+  -webkit-overflow-scrolling: touch;
 }
-ul {
-  list-style-type: none;
+
+html {
+  position: absolute;
+  top: 0;
+  left: 0;
   padding: 0;
+  overflow: auto;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+body {
+  padding: 1rem;
+  overflow: auto;
 }
 </style>
